dde61ccde5138eb1f790a719b7c4cb8a22d6bda4
Saish Menon <saishmenon001@gmail.com>
2023-05-21 00:57:19 -0700

    Updating success messages for page creation.

--- public/code.js
+++ public/code.js
@@ -57,13 +57,14 @@ const pages = {
 
 figma.showUI(__html__, { themeColors: true, width: 256, height: 300 });
 figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-pages') {
+    if (msg.type === "create-pages") {
         if (msg.template === "simple") {
             for (let page of pages.simple.slice(1)) {
                 let newPage = figma.createPage();
                 newPage.name = page;
             }
             figma.currentPage.name = pages.simple[0];
+            figma.notify("✅ Successfully created a simple template for you!");
         }
         else if (msg.template === "intermediate") {
             for (let page of pages.intermediate.slice(1)) {
@@ -71,6 +72,7 @@ figma.ui.onmessage = (msg) => {
                 newPage.name = page;
             }
             figma.currentPage.name = pages.intermediate[0];
+            figma.notify("✅ Your intermediate project has been setup successfully!");
         }
         else if (msg.template === "advanced") {
             for (let page of pages.advanced.slice(1)) {
@@ -78,8 +80,8 @@ figma.ui.onmessage = (msg) => {
                 newPage.name = page;
             }
             figma.currentPage.name = pages.advanced[0];
+            figma.notify("✅ Your Advanced project has been setup successfully!");
         }
-        figma.notify("✅ Pages created!");
     }
     figma.closePlugin();
 };
